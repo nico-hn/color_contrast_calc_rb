4af9dfbf767d24ba5bc7b4aeed4f392ef6f24bfd
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-08-24 08:49:00 +0900

    add test cases for ThresholdFinder.threshold_criteria()

--- spec/threshold_finder_spec.rb
+++ spec/threshold_finder_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe ColorContrastCalc::ThresholdFinder do
     target = 4.5
     orange = Color.from_hex('orange')
     yellow = Color.from_name('yellow')
+    darkgreen = Color.from_name('darkgreen')
 
     context 'when two colors are different' do
       it 'expects to return a ToDarkerSide when yellow and orange are passed' do
@@ -22,6 +23,22 @@ RSpec.describe ColorContrastCalc::ThresholdFinder do
 
       it 'expects to return a ToBrighterSide when orange and yellow are passed' do
         criteria = ThresholdFinder.threshold_criteria(target, orange, yellow)
+        expect(criteria).to be_instance_of(ThresholdCriteria::ToBrighterSide)
+        expect(criteria.increment_condition(4.25)).to be true
+        expect(criteria.round(4.25)).to be 4.3
+      end
+    end
+
+    context 'when two colors are same' do
+      it 'expects to return a ToDarkerSide when yellow is passed' do
+        criteria = ThresholdFinder.threshold_criteria(target, yellow, yellow)
+        expect(criteria).to be_instance_of(ThresholdCriteria::ToDarkerSide)
+        expect(criteria.increment_condition(4.25)).to be false
+        expect(criteria.round(4.25)).to be 4.2
+      end
+
+      it 'expects to return a ToBrighterSide when darkgreen are passed' do
+        criteria = ThresholdFinder.threshold_criteria(target, darkgreen, darkgreen)
         expect(criteria).to be_instance_of(ThresholdCriteria::ToBrighterSide)
         expect(criteria.increment_condition(4.25)).to be true
         expect(criteria.round(4.25)).to be 4.3
