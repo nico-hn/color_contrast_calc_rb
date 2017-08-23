823ea14e11b5fe04d82c3713dcc3f1f4a8f2a85a
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-08-23 10:50:36 +0900

    refactoring of Converter.rgb_map()

--- lib/color_contrast_calc/converter.rb
+++ lib/color_contrast_calc/converter.rb
@@ -4,21 +4,15 @@ require 'matrix'
 
 module ColorContrastCalc
   module Converter
-    def self.clamp_to_range(val, lower_bound, upper_bound)
-      return lower_bound if val <= lower_bound
-      return upper_bound if val > upper_bound
-      val
-    end
-
     def self.rgb_map(vals)
       if block_given?
         return vals.map do |val|
           new_val = yield val
-          clamp_to_range(new_val.round, 0, 255)
+          new_val.round.clamp(0, 255)
         end
       end
 
-      vals.map {|val| clamp_to_range(val.round, 0, 255) }
+      vals.map {|val| val.round.clamp(0, 255) }
     end
 
     module Contrast
