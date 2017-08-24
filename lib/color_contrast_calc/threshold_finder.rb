ef043fd665e3bcd89025cb3511f074f22e42dc6e
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-08-24 20:34:50 +0900

    add ThresholdFinder::Brightness.find()

--- lib/color_contrast_calc/threshold_finder.rb
+++ lib/color_contrast_calc/threshold_finder.rb
@@ -58,6 +58,55 @@ module ColorContrastCalc
     end
 
     module Brightness
+      def self.find(fixed_color, other_color, level = Checker::Level::AA)
+        target_ratio = Checker.level_to_ratio(level)
+        criteria = ThresholdFinder.threshold_criteria(target_ratio,
+                                                      fixed_color, other_color)
+        w = calc_upper_ratio_limit(other_color) / 2.0
+        upper_color = other_color.new_brightness_color(w * 2)
+
+        if other_color.higher_luminance_than?(fixed_color) &&
+            !upper_color.sufficient_contrast?(fixed_color, level)
+          return upper_color
+        end
+
+        r, last_sufficient_r = calc_brightness_ratio(fixed_color, other_color,
+                                                     target_ratio, criteria, w)
+
+        nearest_color = other_color.new_brightness_color(criteria.round(r))
+
+        if last_sufficient_r &&
+            nearest_color.contrast_ratio_against(fixed_color) < target_ratio
+          return other_color.new_brightness_color(criteria.round(last_sufficient_r))
+        end
+
+        nearest_color
+      end
+
+      def self.calc_brightness_ratio(fixed_color, other_color, target_ratio,
+                                     criteria, w)
+        fixed_luminace = fixed_color.relative_luminance
+        other_rgb = other_color.rgb
+        r = w
+        last_sufficient_r = nil
+
+        ThresholdFinder.binary_search_width(w, 0.01) do |d|
+          new_rgb = Converter::Brightness.calc_rgb(other_rgb, r)
+          new_luminace = Checker.relative_luminance(new_rgb)
+          contrast_ratio = Checker.luminance_to_contrast_ratio(fixed_luminace,
+                                                               new_luminace)
+
+          last_sufficient_r = r if contrast_ratio >= target_ratio
+          break if contrast_ratio == target_ratio
+
+          r += criteria.increment_condition(contrast_ratio) ? d : -d
+        end
+
+        [r, last_sufficient_r]
+      end
+
+      private_class_method :calc_brightness_ratio
+
       def self.calc_upper_ratio_limit(color)
         return 100 if color.same_color?(Color::BLACK)
         darkest = color.rgb.reject(&:zero?).min
--- spec/threshold_finder_spec.rb
+++ spec/threshold_finder_spec.rb
@@ -57,6 +57,141 @@ RSpec.describe ColorContrastCalc::ThresholdFinder do
   end
 
   describe ColorContrastCalc::ThresholdFinder::Brightness do
+    describe '.find' do
+      white = Color::WHITE
+      black = Color::BLACK
+      brown = Color.from_name('brown')
+      orange = Color.from_name('orange')
+      mintcream = Color.from_name('mintcream')
+      yellow = Color.from_name('yellow')
+      springgreen = Color.from_name('springgreen')
+      green = Color.from_name('green')
+      darkgreen = Color.from_name('darkgreen')
+      blue = Color.from_name('blue')
+      azure = Color.from_name('azure')
+      blueviolet = Color.from_name('blueviolet')
+      fuchsia = Color.from_name('fuchsia')
+
+      context 'when then fixed color is orange' do
+        it 'expects to return a darker orange when orange is passed' do
+          new_color = Brightness.find(orange, orange)
+          new_contrast_ratio = orange.contrast_ratio_against(new_color)
+
+          expect(orange.contrast_ratio_against(orange)).to be < 4.5
+          expect(new_contrast_ratio).to be > 4.5
+          expect(new_contrast_ratio).to within(0.5).of(4.5)
+          expect(new_color.hex).to eq('#674200')
+        end
+
+        it 'expects to return a darker color when blueviolet is passed' do
+          new_color = Brightness.find(orange, blueviolet)
+          new_contrast_ratio = orange.contrast_ratio_against(new_color)
+
+          expect(new_contrast_ratio).to be > 4.5
+          expect(new_contrast_ratio).to within(0.5).of(4.5)
+          expect(new_color.hex).to eq('#6720a9')
+        end
+      end
+
+      context 'when the color to be adjusted is orange' do
+        it 'expect to return a brigher orange with blue as fixed color' do
+          new_color = Brightness.find(blue, orange)
+          new_contrast_ratio = blue.contrast_ratio_against(new_color)
+
+          expect(new_contrast_ratio).to be > 4.5
+          expect(new_contrast_ratio).to within(0.5).of(4.5)
+          expect(new_color.hex).to eq('#ffaa00')
+        end
+
+        it 'expect to return a brigher orange with blueviolet as fixed color' do
+          new_color = Brightness.find(blueviolet, orange)
+          new_contrast_ratio = blueviolet.contrast_ratio_against(new_color)
+
+          expect(new_contrast_ratio).to be > 4.5
+          expect(new_contrast_ratio).to within(0.5).of(4.5)
+          expect(new_color.hex).to eq('#ffe000')
+        end
+      end
+
+      it 'expects to return a brighter color when brown is passed to brown' do
+        new_color = Brightness.find(brown, brown)
+        new_contrast_ratio = brown.contrast_ratio_against(new_color)
+
+        expect(brown.hex).to eq('#a52a2a')
+        expect(new_contrast_ratio).to be > 4.5
+        expect(new_contrast_ratio).to within(0.5).of(4.5)
+        expect(new_color.hex).to eq('#ffbebe')
+      end
+
+      context 'when darkgreen is passed to white' do
+        it 'expect return a darker green - AA' do
+          new_color = Brightness.find(white, darkgreen)
+          new_contrast_ratio = white.contrast_ratio_against(new_color)
+
+          expect(new_contrast_ratio).to be > 4.5
+          expect(new_contrast_ratio).to within(0.5).of(4.5)
+        end
+
+        it 'expect return a darker green - AAA' do
+          new_color = Brightness.find(white, darkgreen, 'AAA')
+          new_contrast_ratio = white.contrast_ratio_against(new_color)
+
+          expect(new_contrast_ratio).to be > 7.0
+          expect(new_contrast_ratio).to within(0.5).of(7)
+        end
+      end
+
+      it 'expects to return black for AAA if blue is passed to green' do
+        new_color = Brightness.find(green ,blue)
+        expect(new_color.same_color?(black)).to be true
+      end
+
+      context 'when mintcream is passed to yellow' do
+        it 'expects mintcream to be brighter than yellow' do
+          expect(mintcream.higher_luminance_than?(yellow)).to be true
+        end
+
+        it 'expects the upper ratio limit of mintcream to be 105' do
+          new_color = mintcream.new_brightness_color(105)
+
+          expect(Brightness.calc_upper_ratio_limit(mintcream)).to be 105
+          expect(new_color.same_color?(white)).to be true
+        end
+
+        it 'expects to return white when the contrast ratio of A is given' do
+          new_color = Brightness.find(yellow, mintcream, 'A')
+
+          expect(new_color.same_color?(white)).to be true
+        end
+
+        it 'expects to return white when the contrast ratio of AA is given' do
+          new_color = Brightness.find(yellow, mintcream, 'AA')
+
+          expect(new_color.same_color?(white)).to be true
+        end
+
+        it 'expects to return white when the contrast ratio of AAA is given' do
+          new_color = Brightness.find(yellow, mintcream, 'AA')
+
+          expect(new_color.same_color?(white)).to be true
+        end
+      end
+
+      it 'expects to return darker green when springgreen is passed to green' do
+        new_color = Brightness.find(green, springgreen, 'A')
+
+        expect(springgreen.higher_luminance_than?(new_color)).to be true
+        expect(green.contrast_ratio_against(new_color)).to within(0.5).of(3.0)
+      end
+
+      it 'expects to return a darker color when azure is passed to fuchsia' do
+        new_color = Brightness.find(fuchsia, azure, 'A')
+
+        expect(azure.higher_luminance_than?(new_color)).to be true
+        expect(fuchsia.contrast_ratio_against(new_color)).to within(0.5).of(3.0)
+      end
+    end
+
     describe '.calc_upper_ratio_limit' do
       it 'expects to return 100 for black' do
         color = Color.from_name('black')
