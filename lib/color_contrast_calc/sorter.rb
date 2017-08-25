dd03ea2b2831dfb80d4043ee1b9b31bbee4ae0d6
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-08-26 06:48:44 +0900

    add module Sorter.colorcomponent_pos()

--- lib/color_contrast_calc/sorter.rb
+++ lib/color_contrast_calc/sorter.rb
@@ -6,5 +6,11 @@ module ColorContrastCalc
       RGB = 'rgb'.chars
       HSL = 'hsl'.chars
     end
+
+    def self.color_component_pos(components, default_components)
+      components.downcase.chars.map do |c|
+        default_components.index(c)
+      end
+    end
   end
 end
--- spec/sorter_spec.rb
+++ spec/sorter_spec.rb
@@ -6,4 +6,29 @@ Color = ColorContrastCalc::Color
 Sorter = ColorContrastCalc::Sorter
 
 RSpec.describe ColorContrastCalc::Sorter do
+  describe '.color_component_pos' do
+    context 'when components of hsl are given' do
+      it 'expect to return [0, 1, 2] when hsl is passed' do
+        pos = Sorter.color_component_pos('hsl', Sorter::ColorComponent::HSL)
+        expect(pos).to eq([0, 1, 2])
+      end
+
+      it 'expect to return [0, 2, 1] when hLs is passed' do
+        pos = Sorter.color_component_pos('hLs', Sorter::ColorComponent::HSL)
+        expect(pos).to eq([0, 2, 1])
+      end
+    end
+
+    context 'when components of rgb are given' do
+      it 'expect to return [0, 1, 2] when rgb is passed' do
+        pos = Sorter.color_component_pos('rgb', Sorter::ColorComponent::RGB)
+        expect(pos).to eq([0, 1, 2])
+      end
+
+      it 'expect to return [2, 1, 0] when bgr is passed' do
+        pos = Sorter.color_component_pos('bgr', Sorter::ColorComponent::RGB)
+        expect(pos).to eq([2, 1, 0])
+      end
+    end
+  end
 end
