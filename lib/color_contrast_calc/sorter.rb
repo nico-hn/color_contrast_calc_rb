412285195c8c4eab029137114281a83818150817
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-08-26 06:45:22 +0900

    add module ColorContrastCalc::Sorter

---
+++ lib/color_contrast_calc/sorter.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+module ColorContrastCalc
+  module Sorter
+    module ColorComponent
+      RGB = 'rgb'.chars
+      HSL = 'hsl'.chars
+    end
+  end
+end
---
+++ spec/sorter_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require 'color_contrast_calc/color'
+require 'color_contrast_calc/sorter'
+
+Color = ColorContrastCalc::Color
+Sorter = ColorContrastCalc::Sorter
+
+RSpec.describe ColorContrastCalc::Sorter do
+end
