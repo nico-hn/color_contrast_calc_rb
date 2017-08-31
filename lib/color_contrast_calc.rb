ba08b09b7deecf8935d86c78e5d1388c55464e80
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-08-31 09:16:19 +0900

    update lib/color_contrast_calc.rb: require submodules

--- lib/color_contrast_calc.rb
+++ lib/color_contrast_calc.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 require 'color_contrast_calc/version'
+require 'color_contrast_calc/utils'
+require 'color_contrast_calc/converter'
+require 'color_contrast_calc/checker'
+require 'color_contrast_calc/threshold_finder'
+require 'color_contrast_calc/color'
+require 'color_contrast_calc/sorter'
 
 module ColorContrastCalc
   # Your code goes here...
