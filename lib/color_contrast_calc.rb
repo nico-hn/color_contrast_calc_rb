2e65dd39d36e65d7a460357538c6a651794d5439
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-11-02 22:56:10 +0900

    add ColorContrastCalc.color_from()

--- lib/color_contrast_calc.rb
+++ lib/color_contrast_calc.rb
@@ -9,5 +9,46 @@ require 'color_contrast_calc/color'
 require 'color_contrast_calc/sorter'
 
 module ColorContrastCalc
-  # Your code goes here...
+  class InvalidColorRepresentationError < StandardError; end
+
+  def self.color_from(color_value, name = nil)
+    error_message = 'A color should be given as an array or string.'
+
+    if !color_value.is_a?(String) && !color_value.is_a?(Array)
+      raise InvalidColorRepresentationError, error_message
+    end
+
+    return color_from_rgb(color_value, name) if color_value.is_a?(Array)
+    color_from_str(color_value, name)
+  end
+
+  def self.color_from_rgb(color_value, name = nil)
+    error_message = 'A RGB value should be given in form of [r, g, b].'
+
+    unless Utils.valid_rgb?(color_value)
+      raise InvalidColorRepresentationError, error_message
+    end
+
+    hex_code = Utils.rgb_to_hex(color_value)
+    Color::List::HEX_TO_COLOR[hex_code] || Color.new(color_value, name)
+  end
+
+  private_class_method :color_from_rgb
+
+  def self.color_from_str(color_value, name = nil)
+    error_message = 'A hex code is in form of "#xxxxxx" where 0 <= x <= f.'
+
+    named_color = Color::List::NAME_TO_COLOR[color_value]
+    return named_color if named_color
+
+    if Utils.valid_hex?(color_value)
+      hex_code = Utils.normalize_hex(color_value)
+    else
+      raise InvalidColorRepresentationError, error_message
+    end
+
+    Color::List::HEX_TO_COLOR[hex_code] || Color.new(hex_code, name)
+  end
+
+  private_class_method :color_from_str
 end
--- spec/color_contrast_calc_spec.rb
+++ spec/color_contrast_calc_spec.rb
@@ -4,4 +4,57 @@ RSpec.describe ColorContrastCalc do
   it 'has a version number' do
     expect(ColorContrastCalc::VERSION).not_to be nil
   end
+
+  describe '.color_from' do
+    yellow_name = 'yellow'
+    yellow_hex = '#ffff00'
+    yellow_short_hex = '#ff0'
+    yellow_rgb = [255, 255, 0]
+    invalid_name = 'imaginaryblue'
+    invalid_hex = '#ff00'
+    invalid_rgb = [255, 256, 0]
+    error = ColorContrastCalc::InvalidColorRepresentationError
+
+    it 'is expected to return an instance of Color when "yellow" is passed' do
+      yellow = ColorContrastCalc.color_from(yellow_name)
+      expect(yellow.hex).to eq(yellow_hex)
+      expect(yellow.name).to eq(yellow_name)
+    end
+
+    it 'is expected to return an instance of Color when "#ffff00" is passed' do
+      yellow = ColorContrastCalc.color_from(yellow_hex)
+      expect(yellow.hex).to eq(yellow_hex)
+      expect(yellow.name).to eq(yellow_name)
+    end
+
+    it 'is expected to return an instance of Color when "#ff0" is passed' do
+      yellow = ColorContrastCalc.color_from(yellow_short_hex)
+      expect(yellow.hex).to eq(yellow_hex)
+      expect(yellow.name).to eq(yellow_name)
+    end
+
+    it 'is expected to return an instance of Color when [255, 255, 0] is passed' do
+      yellow = ColorContrastCalc.color_from(yellow_rgb)
+      expect(yellow.hex).to eq(yellow_hex)
+      expect(yellow.name).to eq(yellow_name)
+    end
+
+    it 'is expected to raise an error when "imaginaryblue" is passed' do
+      expect {
+        ColorContrastCalc.color_from(invalid_name)
+      }.to raise_error(error)
+    end
+
+    it 'is expected to raise an error when "#ff00" is passed' do
+      expect {
+        ColorContrastCalc.color_from(invalid_hex)
+      }.to raise_error(error)
+    end
+
+    it 'is expected to raise an error when [255, 256, 0] is passed' do
+      expect {
+        ColorContrastCalc.color_from(invalid_rgb)
+      }.to raise_error(error)
+    end
+  end
 end
