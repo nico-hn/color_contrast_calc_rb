3b00e896813416adb22b60fcf5d75e8d5ab55f8e
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-11-12 23:08:34 +0900

    add documentation comments to methods in shim.rb

--- lib/color_contrast_calc/shim.rb
+++ lib/color_contrast_calc/shim.rb
@@ -2,6 +2,11 @@
 
 unless //.respond_to? :match?
   class Regexp
+    ##
+    # Regexp.match?() is available for Ruby >= 2.4,
+    # and the following implementation does not satisfy
+    # the full specification of the original method.
+
     def match?(str)
       self === str
     end
@@ -10,6 +15,11 @@ end
 
 unless 0.respond_to? :clamp
   class Numeric
+    ##
+    # Comparable#clamp() is available for Ruby >= 2.4,
+    # and the following implementation does not satisfy
+    # the full specification of the original method.
+
     def clamp(lower_bound, upper_bound)
       return lower_bound if self < lower_bound
       return upper_bound if self > upper_bound
