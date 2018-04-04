215c9ffb6783222df6b09c29fcaf8954e9fb4bd9
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2018-04-04 18:20:06 +0900

    extract method ColorGroup.hue_rotated_colors()

--- lib/color_contrast_calc/color_group.rb
+++ lib/color_contrast_calc/color_group.rb
@@ -7,14 +7,20 @@ module ColorContrastCalc
     def self.analogous(main_color, degree = 15)
       main = ColorContrastCalc.color_from(main_color)
       main_hsl = main.hsl
+      colors = hue_rotated_colors(main_hsl, [-1, 0, 1], degree)
+      new(colors)
+    end
+
+    def self.hue_rotated_colors(main_hsl, rotation_rates, degree)
       main_hue = main_hsl[0]
-      colors = [-1, 0, 1].map do |i|
+      rotation_rates.map do |i|
         hsl = main_hsl.dup
         hsl[0] = (360 + main_hue + degree * i) % 360
         Color.new_from_hsl(hsl)
       end
-      new(colors)
     end
+
+    private_class_method :hue_rotated_colors
 
     attr_reader :colors
 
