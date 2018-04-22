053ac3719086844b47a9a385e9ad95dd0b8d3bce
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2018-04-22 12:10:54 +0900

    redefine Color#new_*_color() as aliases of Color#with_*()

---
+++ lib/color_contrast_calc/deprecated.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module ColorContrastCalc
+  ##
+  # Collection of deprecated methods.
+
+  module Deprecated
+    module Color
+      # @deprecated Use {#with_contrast} instead
+      def new_contrast_color(ratio, name = nil)
+        with_contrast(ratio, name)
+      end
+
+      # @deprecated Use {#with_brightness} instead
+      def new_brightness_color(ratio, name = nil)
+        with_brightness(ratio, name)
+      end
+
+      # @deprecated Use {#with_invert} instead
+      def new_invert_color(ratio = 100, name = nil)
+        with_invert(ratio, name)
+      end
+
+      # @deprecated Use {#with_hue_rotate} instead
+      def new_hue_rotate_color(degree, name = nil)
+        with_hue_rotate(degree, name)
+      end
+
+      # @deprecated Use {#with_saturate} instead
+      def new_saturate_color(ratio, name = nil)
+        with_saturate(ratio, name)
+      end
+
+      # @deprecated Use {#with_grayscale} instead
+      def new_grayscale_color(ratio = 100, name = nil)
+        with_grayscale(ratio, name)
+      end
+    end
+  end
+end
