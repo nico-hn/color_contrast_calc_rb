acb375764beef6fa81bda221ba851aba263c34aa
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2020-01-09 22:16:24 +0900

    group test cases related to ColorFunctionParser::Parser

--- spec/color_function_parser_spec.rb
+++ spec/color_function_parser_spec.rb
@@ -53,118 +53,120 @@ ERROR
     end
   end
 
-  describe '.skip_spaces!' do
-    it 'expects to skip the spaces at the current scan pointer.' do
-      str_with_2_spaces = StringScanner.new('  a string with spaces at the head')
+  describe ColorContrastCalc::ColorFunctionParser::Parser do
+    describe '.skip_spaces!' do
+      it 'expects to skip the spaces at the current scan pointer.' do
+        str_with_2_spaces = StringScanner.new('  a string with spaces at the head')
 
-      2.times do
-        Parser.send :skip_spaces!, str_with_2_spaces
-        expect(str_with_2_spaces.charpos).to eq(2)
+        2.times do
+          Parser.send :skip_spaces!, str_with_2_spaces
+          expect(str_with_2_spaces.charpos).to eq(2)
+        end
       end
     end
-  end
 
-  describe '.read_scheme!' do
-    it 'expects to raise an error for a wrong scheme' do
-      message = <<ERROR
+    describe '.read_scheme!' do
+      it 'expects to raise an error for a wrong scheme' do
+        message = <<ERROR
 "rjb(255, 255, 255)" is not a valid code. An error occurred at:
 rjb(255, 255, 255)
 ^ while searching with (?i-mx:(rgb|hsl|hwb))
 ERROR
-      wrong_white = StringScanner.new('rjb(255, 255, 255)')
-      expect {
-        Parser.send :read_scheme!, wrong_white
-      }.to raise_error(error, message)
-    end
+        wrong_white = StringScanner.new('rjb(255, 255, 255)')
+        expect {
+          Parser.send :read_scheme!, wrong_white
+        }.to raise_error(error, message)
+      end
 
-    it 'expects to raise an error if the open parenthesis is missing' do
-      message = <<ERROR
+      it 'expects to raise an error if the open parenthesis is missing' do
+        message = <<ERROR
 "rgb255, 255, 255)" is not a valid code. An error occurred at:
 rgb255, 255, 255)
    ^ while searching with (?-mix:\\()
 ERROR
-      wrong_white = StringScanner.new('rgb255, 255, 255)')
-      expect {
-        Parser.send :read_scheme!, wrong_white
-      }.to raise_error(error, message)
-    end
-
-    it 'expects to read a valid scheme' do
-      valid_whites = [
-        'rgb(255, 255, 255)',
-        'RGB(255, 255, 255)',
-        'rgb(255,255,255)',
-        'rgb( 255,255,255)',
-        'rgb(255 , 255,255)',
-        'rgb(255,255,255 )',
-        'rgb(255 255 255)',
-        'rgb(255 255 255 )',
-        'rgb( 255 255 255 )'
-      ]
-      expected = {
-        scheme: Scheme::RGB,
-        parameters: [
-          { number: '255', unit: nil },
-          { number: '255', unit: nil },
-          { number: '255', unit: nil }
-        ]
-      }
-
-      valid_whites.each do |val|
-        white = StringScanner.new(val)
-        result = Parser.send(:read_scheme!, white)
-        expect(result).to eq(expected)
+        wrong_white = StringScanner.new('rgb255, 255, 255)')
+        expect {
+          Parser.send :read_scheme!, wrong_white
+        }.to raise_error(error, message)
       end
-    end
 
-    it 'expects to read a valid hsl values' do
-      valid_hsls = ['hsl(60deg, 100%, 50%)', 'HSL(60.0deg, 25.0%, 50.0%)']
-      expected_values = [
-        {
-          scheme: Scheme::HSL,
+      it 'expects to read a valid scheme' do
+        valid_whites = [
+          'rgb(255, 255, 255)',
+          'RGB(255, 255, 255)',
+          'rgb(255,255,255)',
+          'rgb( 255,255,255)',
+          'rgb(255 , 255,255)',
+          'rgb(255,255,255 )',
+          'rgb(255 255 255)',
+          'rgb(255 255 255 )',
+          'rgb( 255 255 255 )'
+        ]
+        expected = {
+          scheme: Scheme::RGB,
           parameters: [
-            { number: '60', unit: 'deg' },
-            { number: '100', unit: '%' },
-            { number: '50', unit: '%' }
-          ]
-        },
-        {
-          scheme: Scheme::HSL,
-          parameters: [
-            { number: '60.0', unit: 'deg' },
-            { number: '25.0', unit: '%' },
-            { number: '50.0', unit: '%' }
+            { number: '255', unit: nil },
+            { number: '255', unit: nil },
+            { number: '255', unit: nil }
           ]
         }
-      ]
-      valid_hsls.zip(expected_values) do |hsl, expected|
-        result = Parser.send(:read_scheme!, StringScanner.new(hsl))
-        expect(result).to eq(expected)
+
+        valid_whites.each do |val|
+          white = StringScanner.new(val)
+          result = Parser.send(:read_scheme!, white)
+          expect(result).to eq(expected)
+        end
       end
-    end
 
-    it 'expects to read valid HWB functions' do
-      valid_whites = [
-        'hwb(60 0% 0%)',
-        'HWB(60 0% 0%)',
-        'hwb(60  0%  0%)',
-        'hwb( 60 0% 0%)',
-        'hwb(60 0% 0% )',
-        'hwb( 60 0% 0% )'
-      ]
-      expected = {
-        scheme: Scheme::HWB,
-        parameters: [
-          { number: '60', unit: nil },
-          { number: '0', unit: '%' },
-          { number: '0', unit: '%' }
+      it 'expects to read a valid hsl values' do
+        valid_hsls = ['hsl(60deg, 100%, 50%)', 'HSL(60.0deg, 25.0%, 50.0%)']
+        expected_values = [
+          {
+            scheme: Scheme::HSL,
+            parameters: [
+              { number: '60', unit: 'deg' },
+              { number: '100', unit: '%' },
+              { number: '50', unit: '%' }
+            ]
+          },
+          {
+            scheme: Scheme::HSL,
+            parameters: [
+              { number: '60.0', unit: 'deg' },
+              { number: '25.0', unit: '%' },
+              { number: '50.0', unit: '%' }
+            ]
+          }
         ]
-      }
+        valid_hsls.zip(expected_values) do |hsl, expected|
+          result = Parser.send(:read_scheme!, StringScanner.new(hsl))
+          expect(result).to eq(expected)
+        end
+      end
 
-      valid_whites.each do |val|
-        white = StringScanner.new(val)
-        result = Parser.send(:read_scheme!, white)
-        expect(result).to eq(expected)
+      it 'expects to read valid HWB functions' do
+        valid_whites = [
+          'hwb(60 0% 0%)',
+          'HWB(60 0% 0%)',
+          'hwb(60  0%  0%)',
+          'hwb( 60 0% 0%)',
+          'hwb(60 0% 0% )',
+          'hwb( 60 0% 0% )'
+        ]
+        expected = {
+          scheme: Scheme::HWB,
+          parameters: [
+            { number: '60', unit: nil },
+            { number: '0', unit: '%' },
+            { number: '0', unit: '%' }
+          ]
+        }
+
+        valid_whites.each do |val|
+          white = StringScanner.new(val)
+          result = Parser.send(:read_scheme!, white)
+          expect(result).to eq(expected)
+        end
       end
     end
   end
