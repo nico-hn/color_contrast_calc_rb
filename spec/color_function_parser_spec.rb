0db5e5f4ff29c78c19963cfd3eaa217db181a5c0
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2020-01-11 22:08:40 +0900

    correct the description of some test cases

--- spec/color_function_parser_spec.rb
+++ spec/color_function_parser_spec.rb
@@ -383,19 +383,19 @@ TEMPLATE
     deg = Parser.to_rgb('hsl(90deg, 100%, 50%)')
 
     describe 'grad' do
-      it 'expects to be converted to [255, 255, 0]' do
+      it 'expects to be converted to [128, 255, 0]' do
         expect(Parser.to_rgb('hsl(100grad, 100%, 50%)')).to eq(deg)
       end
     end
 
     describe 'turn' do
-      it 'expects to be converted to [255, 255, 0]' do
+      it 'expects to be converted to [128, 255, 0]' do
         expect(Parser.to_rgb('hsl(0.25turn, 100%, 50%)')).to eq(deg)
       end
     end
 
     describe 'turn' do
-      it 'expects to be converted to [255, 255, 0]' do
+      it 'expects to be converted to [128, 255, 0]' do
         expect(Parser.to_rgb('hsl(1.57rad, 100%, 50%)')).to eq(deg)
       end
     end
