54b98b126192c81212d582ceee7782134022773a
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2021-07-29 01:32:47 +0900

    add a script to update rbs files while taking into account their dependencies

---
+++ scripts/generate_rbs_templates_with_existing_rbs.sh
@@ -0,0 +1,8 @@
+for RB_FILE in `cat sig_gen/lib_files_without_rbs.txt`
+do
+    TMP_RBS=sig_gen/rgb.tmp
+    SIG_FILE=`echo $RB_FILE | sed -e 's/^lib/sig_gen/' -e 's/\.rb$/.gen.rbs/'`
+    echo ==== $RB_FILE ====
+    typeprof $RB_FILE `find sig_gen -type f -name '*.gen.rbs'` > $TMP_RBS
+    mv $TMP_RBS $SIG_FILE
+done
