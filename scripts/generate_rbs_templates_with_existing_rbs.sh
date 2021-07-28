ddd9afdcd57e60a2f4d37388db4256897c3d3b16
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2021-07-29 02:26:51 +0900

    update generate_rbs_templates_with_existing_rbs.sh for using manually configured rbs files

--- scripts/generate_rbs_templates_with_existing_rbs.sh
+++ scripts/generate_rbs_templates_with_existing_rbs.sh
@@ -3,6 +3,6 @@ do
     TMP_RBS=sig_gen/rgb.tmp
     SIG_FILE=`echo $RB_FILE | sed -e 's/^lib/sig_gen/' -e 's/\.rb$/.gen.rbs/'`
     echo ==== $RB_FILE ====
-    typeprof $RB_FILE `find sig_gen -type f -name '*.gen.rbs'` > $TMP_RBS
+    typeprof $RB_FILE `find sig -type f -name '*.rbs'` `find sig_gen -type f -name '*.gen.rbs'` > $TMP_RBS
     mv $TMP_RBS $SIG_FILE
 done
